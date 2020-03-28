7755e6b493a3e7fb3abaa2bc927399f157f87fc3
mkonopkait <mkonopka.it@gmail.com>
2020-03-28 15:11:33 +0100

    Add project files.

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        {
+            return await this._context.Product.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await this._context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await this._context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            this._context.Product.Add(product);
+            await this._context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await this._context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            this._context.Product.Remove(product);
+            await this._context.SaveChangesAsync();
+
+            return product;
+        }
+
+        private bool ProductExists(int id)
+        {
+            return this._context.Product.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApi/Helpers/SeedDatabase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Model;
+
+namespace WebApi.Helpers
+{
+    public class SeedDatabase
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AppDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<AppDbContext>>()))
+            {
+                if (context.Product.Any())
+                {
+                    return;
+                }
+                Product[] products =
+                {
+                    new Product()
+                    {
+                        Name = "Chleb",
+                        Price = 2.4m
+                    },
+                    new Product()
+                    {
+                        Name = "Mleko",
+                        Price = 1.6m
+                    }
+                };
+                context.Product.AddRange(products);
+                context.SaveChanges();
+            }
+        }
+    }
+}
