6bd709438bdc1b41e456984fae7a715515c878e1
mkonopkait <mkonopka.it@gmail.com>
2020-03-28 20:04:18 +0100

    changed root directory

---
+++ WebApi/Model/Invoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+
+        public int Number { get; set; }
+
+        public int MyProperty { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public DateTime SoldDate { get; set; }
+
+        public string IssuePlace { get; set; }
+
+        public string Seller { get; set; }
+
+        public string Purchaser { get; set; }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -31,11 +31,17 @@ namespace WebApi
               options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
